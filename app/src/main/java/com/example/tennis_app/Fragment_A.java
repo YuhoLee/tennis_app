8ffdd2b16de20ea9ac2a1c54b11b28db5a1d3a4a
YuhoLee <cdcdefg4@gmail.com>
2022-04-14 13:08:58 +0900

    - Bluetooth update

--- app/src/main/java/com/example/tennis_app/Fragment_A.java
+++ app/src/main/java/com/example/tennis_app/Fragment_A.java
@@ -1,20 +1,94 @@
 package com.example.tennis_app;
 
+import static android.content.ContentValues.TAG;
+
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+
 public class Fragment_A extends Fragment {
+    private static final int REQUEST_ENABLE_BT = 10; // 블루투스 활성화 상태
+    private BluetoothAdapter bluetoothAdapter; // 블루투스 어댑터
+    private Set<BluetoothDevice> devices; // 블루투스 디바이스 데이터 셋
+    private BluetoothDevice bluetoothDevice; // 블루투스 디바이스
+    private BluetoothSocket bluetoothSocket = null; // 블루투스 소켓
+    private OutputStream outputStream = null; // 블루투스에 데이터를 출력하기 위한 출력 스트림
+    private InputStream inputStream = null; // 블루투스에 데이터를 입력하기 위한 입력 스트림
+    private Thread workerThread = null; // 문자열 수신에 사용되는 쓰레드
+    private byte[] readBuffer; // 수신 된 문자열을 저장하기 위한 버퍼
+    private int readBufferPosition; // 버퍼 내 문자 저장 위치
+    private TextView textViewReceive; // 수신 된 데이터를 표시하기 위한 텍스트 뷰
+    private EditText editTextSend; // 송신 할 데이터를 작성하기 위한 에딧 텍스트
+    private Button buttonSend; // 송신하기 위한 버튼
+    private ActivityResultLauncher<String> requestPermissionLauncher;
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_a, container, false);
+        View v = inflater.inflate(R.layout.fragment_a, container, false);
+        checkPermission();
+        // 각 컨테이너들의 id를 매인 xml과 맞춰준다.
+        textViewReceive = v.findViewById(R.id.textView_receive);
+        editTextSend = v.findViewById(R.id.editText_send);
+        buttonSend = v.findViewById(R.id.button_send);
+
+        // 블루투스 활성화 하기
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter(); // 블루투스 어댑터를 디폴트 어댑터로 설정
+
+        if(bluetoothAdapter == null) {   // 디바이스가 블루투스 지원하지 않을 시
+        }
+        else {   // 디바이스가 블루투스 지원할 시
+            if (bluetoothAdapter.isEnabled()) {   // 블루투스가 활성화되었을 때
+                selectBluetoothDevice();    // 블루투스 디바이스 선택 함수
+            } else {   // 블루투스가 비활성화일 때
+                // 블루투스 활성화를 위한 다이얼로그 출력
+                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                // 선택한 값이 onActivityResult 함수에서 콜백된다.
+                ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
+                        new ActivityResultContracts.StartActivityForResult(),
+                        new ActivityResultCallback<ActivityResult>() {
+                            @Override
+                            public void onActivityResult(ActivityResult result) {
+                                if (result.getResultCode() == Activity.RESULT_OK) { // 사용 선택 시
+                                    selectBluetoothDevice();    // 블루투스 디바이스 선택 함수
+                                }
+                                else{   // 취소 시
+
+                                }
+                            }
+                        });
+                startActivityResult.launch(intent);
+
+
+            }
+        }
+        return v;
     }
 
     @Override
@@ -22,6 +96,50 @@ public class Fragment_A extends Fragment {
         super.onCreate(savedInstanceState);
 
 
+    }
 
+    public void checkPermission(){
+         requestPermissionLauncher =
+                registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                    if (isGranted) {
+                        // Permission is granted. Continue the action or workflow in your
+                        // app.
+                    } else {
+                        // Explain to the user that the feature is unavailable because the
+                        // features requires a permission that the user has denied. At the
+                        // same time, respect the user's decision. Don't link to system
+                        // settings in an effort to convince the user to change their
+                        // decision.
+                    }
+                });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            requestPermissions(
+                    new String[]{
+                            Manifest.permission.BLUETOOTH,
+                            Manifest.permission.BLUETOOTH_SCAN,
+                            Manifest.permission.BLUETOOTH_ADVERTISE,
+                            Manifest.permission.BLUETOOTH_CONNECT
+                    },
+                    1);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(
+                    new String[]{
+                            Manifest.permission.BLUETOOTH
+                    },
+                    1);
+        }
+    }
+
+    public void selectBluetoothDevice(){
+        // 이미 페어링 되어있는 블루투스 기기를 찾습니다.
+        try {
+            devices = bluetoothAdapter.getBondedDevices();
+        }
+        catch(SecurityException e){
+            e.printStackTrace();
+        }
+
+        // 페어링 된 디바이스의 크기를 저장
     }
 }
